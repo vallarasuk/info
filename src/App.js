7ae76984fa644207fed9d655ccb0155bc1e50aed
Vallarasu Kanthasamy <vallarasu.kanthasamy@thidiff.com>
2023-07-20 12:33:04 +0530

    update commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          vallarasu_kanthasamy
-        </p>
+        <img
+          src="https://static.vecteezy.com/system/resources/thumbnails/022/636/391/small/update-system-icon-update-system-illustration-symbol-install-sign-or-logo-vector.jpg"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Myself vallarasu_kanthasamy</p>
+        <br />
+        <p>working on updated process</p>
       </header>
     </div>
   );
