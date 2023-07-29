b5ee02aeabe3f633a3ab8b022d7d5c0ad90c93bf
Vallarasu Kanthasamy <vallarasu.kanthasamy@thidiff.com>
2023-07-29 20:08:42 +0530

    removed unwanted files commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import "./App.css";
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Myself vallarasu_kanthasamy</p>
-        <br />
-        <p>working on updated process</p>
-        Click <a href="https://wa.me/+918122992143">Hear</a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
