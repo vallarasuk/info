451115b5e05edcd6153378c7f29b34d675a2fdb3
Vallarasu Kanthasamy <vallarasu.kanthasamy@thidiff.com>
2023-08-10 11:51:01 +0530

    Updates

--- static/js/472.6bca970d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinfo=self.webpackChunkinfo||[]).push([[472],{5472:function(n,e,t){t.r(e);t(2791);var u=t(184);e.default=function(){return(0,u.jsx)("div",{children:"Contact"})}}}]);
-//# sourceMappingURL=472.6bca970d.chunk.js.map
