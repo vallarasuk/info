d0fb05b224fc5ee5f6199de447b0cf241504a29d
Vallarasu Kanthasamy <vallarasu.kanthasamy@thidiff.com>
2023-08-11 14:52:51 +0530

    SEO optimized version commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,15 +3,13 @@ import { Helmet } from "react-helmet-async";
 import { animated, useSpring } from "react-spring";
 import { Link } from "react-router-dom";
 import "./Styles/Home.css";
-import HomeContent from "./Constant/HomeContent"; // Import the HomeContent
-import Skill from "./Skills"; // Import the Skill component
+import HomeContent from "./Constant/HomeContent";
+import Skill from "./Skills";
 
 const Home = () => {
-  // Destructuring the data from HomeContent
   const {
     title,
     description,
-    // imageURL,
     ctaButtonText,
     ctaButtonLink,
     skills,
@@ -21,7 +19,6 @@ const Home = () => {
     titleIcon,
   } = HomeContent;
 
-  // Text animation configuration
   const textAnimation = useSpring({
     opacity: 1,
     y: 0,
@@ -29,32 +26,21 @@ const Home = () => {
     config: { duration: 1000 },
   });
 
-  // Image animation configuration
-  // const imageAnimation = useSpring({
-  //   opacity: 1,
-  //   scale: 1,
-  //   from: { opacity: 0, scale: 0.9 },
-  //   config: { duration: 1000, delay: 500 },
-  // });
-
   return (
     <>
-      {/* Home Section */}
       <section className="home-section">
-        {/* Helmet for SEO */}
         <Helmet>
-          <title>{name}</title>
+          <title>{name} - React Developer | Frontend Web Developer</title>
           <meta
             name="description"
-            content="Welcome to the portfolio home page of Vallarasu Kanthasamy, a React Front End Developer."
+            content="Welcome to the portfolio home page of Vallarasu Kanthasamy, a talented React Developer and Frontend Web Developer. Explore my portfolio, skills, and contact me for collaboration or inquiries."
           />
-          {/* Add more relevant meta tags for SEO */}
           <meta
             name="keywords"
-            content="portfolio, web development, projects, React, frontend developer, Vallarasu Kanthasamy"
+            content="React developer, Frontend developer, Web developer, Portfolio, Skills, Collaboration, Inquiry"
           />
           <meta name="author" content={`${name} - ${role}`} />
-          <meta property="og:title" content={`${name}`} />
+          <meta property="og:title" content={`${name} - React Developer`} />
           <meta
             property="og:description"
             content="Explore my portfolio and skills in web development. Contact me for collaboration or inquiries."
@@ -64,7 +50,7 @@ const Home = () => {
           <meta property="og:type" content="website" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:creator" content="@your-twitter-handle" />
-          <meta name="twitter:title" content={`${name} - ${role}`} />
+          <meta name="twitter:title" content={`${name} - React Developer`} />
           <meta
             name="twitter:description"
             content="Explore my portfolio and skills in web development. Contact me for collaboration or inquiries."
@@ -76,9 +62,7 @@ const Home = () => {
             href="./Assects/Profile.jpg"
             type="image/x-icon"
           />
-          {/* Set favicon */}
         </Helmet>
-        {/* Content for the home section */}
         <div className="container-fluid">
           <div className="row align-items-center">
             <div className="col-12 text-center">
@@ -87,31 +71,15 @@ const Home = () => {
                 {description.map((paragraph, index) => (
                   <p key={index}>{paragraph}</p>
                 ))}
-                {/* Replace the anchor tag with Link component */}
                 <Link to={ctaButtonLink} className="btn btn-primary">
                   {ctaButtonText}
                 </Link>
               </animated.div>
             </div>
-            {/* Uncomment the following section to display the image */}
-            {/* <div className="col-lg-6">
-              <animated.div style={imageAnimation} className="text-center">
-                <img
-                  src={imageURL}
-                  alt="Your_image"
-                  style={{
-                    width: "100%",
-                    maxWidth: "400px",
-                    borderRadius: "8px",
-                  }}
-                />
-              </animated.div>
-            </div> */}
           </div>
         </div>
       </section>
 
-      {/* Skill Section */}
       <section>
         <Skill skills={skills} skillsTitle={skillsTitle} />
       </section>
