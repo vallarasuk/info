bc865930b68001a5e7c879110edb198994299830
Vallarasu Kanthasamy <vallarasu.kanthasamy@thidiff.com>
2023-08-12 19:23:00 +0530

    feature/content updated commit for the home page section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,8 +1,30 @@
 import React from "react";
-import { motion } from "framer-motion";
 import "./Styles/About.css";
+import StudyTimeline from "./Helper/Studies";
+import Title from "./Helper/Title";
+
 
 const About = () => {
+  // Define your studies data
+  const studies = [
+    {
+      year: "2020 - Present",
+      title: "Master's Degree in Front-End Development",
+      institution: "University of Web Development",
+      description:
+        "Studied advanced front-end development techniques and web design principles.",
+      icon: "graduation", // Use 'graduation' icon for this entry
+    },
+    {
+      year: "2018 - 2020",
+      title: "Bachelor's Degree in Computer Science",
+      institution: "Tech University",
+      description:
+        "Completed coursework in computer science, algorithms, data structures, and software engineering.",
+      icon: "graduation", // Use 'graduation' icon for this entry
+    },
+  ];
+
   return (
     <div className="about-container">
       {/* About Me Section */}
@@ -11,29 +33,46 @@ const About = () => {
           <div className="row">
             <div className="col-12 col-sm-8 offset-sm-2">
               <div className="about-content mt-sm-5">
-                {/* Animation for section heading */}
-                <motion.h2
-                  initial={{ opacity: 0, y: 50 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.8, delay: 0.5 }}
-                  className="about-heading text-center mb-3"
-                >
-                  About Me
-                </motion.h2>
-                {/* Animation for section content */}
-                <motion.p
-                  initial={{ opacity: 0, y: 50 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.8, delay: 0.7 }}
-                  className="about-description"
-                >
+                <Title text="About Me: A Passionate React Developer" />
+                <p className="about-description">
                   Hi, I'm Vallarasu K, a passionate React developer with a
                   strong focus on creating exceptional web experiences. I'm
-                  dedicated to delivering high-quality front-end solutions that
-                  combine technical expertise with creativity. With a background
-                  in computer science and years of hands-on experience, I bring
-                  a unique perspective to every project I work on.
-                </motion.p>
+                  dedicated to delivering high-quality front-end solutions
+                  that combine technical expertise with creativity. With a
+                  background in computer science and years of hands-on
+                  experience, I bring a unique perspective to every project
+                  I work on.
+                </p>
+                {/* Additional content */}
+                <p className="about-description">
+                  I completed my SSLC with 87% from the National Matriculation
+                  School and later pursued a Diploma in Mechanical Engineering.
+                  I have a year of focused learning in front-end development.
+                  Currently, I am thriving in the role of a React Developer at
+                  Thidiff Technology, located in Bangalore. I'm passionate
+                  about creating efficient and user-friendly web applications
+                  that make a meaningful impact.
+                </p>
+                {/* Link to the skills page */}
+                <p className="about-description">
+                  To see a list of my skills and certifications, please visit
+                  the <a href="/skills">Skills Page</a>.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Studies Timeline Section */}
+      <section id="studies" className="studies-section mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-sm-8 offset-sm-2">
+              <div className="studies-content">
+                <Title text={"My Studies"} />
+                {/* Use the StudyTimeline component */}
+                <StudyTimeline studies={studies} />
               </div>
             </div>
           </div>
--- src/Components/Helper/ProfileInfo.jsx
+++ src/Components/Helper/ProfileInfo.jsx
@@ -1,50 +1,41 @@
 import React from "react";
 import { animated, useSpring } from "react-spring";
+import { useInView } from "react-intersection-observer";
 
 const ProfileInfo = ({ name, role, email }) => {
   const animationConfig = {
-    duration: 1500, // Set the duration to 1.5 seconds
+    duration: 1000, // Set the duration to 1 second
     tension: 200,
     friction: 20,
   };
 
-  const topAnimation = useSpring({
-    transform: "translateY(0px)",
-    from: { transform: "translateY(-100px)" },
-    config: animationConfig,
-  });
-
-  const rightAnimation = useSpring({
-    transform: "translateX(0px)",
-    from: { transform: "translateX(100px)" },
-    config: animationConfig,
+  // Use the useInView hook to detect when the component is in the viewport
+  const [bottomRef, bottomInView] = useInView({
+    triggerOnce: true, // Trigger the animation once
+    rootMargin: "-100px", // Add a root margin to start the animation earlier
   });
 
   const bottomAnimation = useSpring({
-    transform: "translateY(0px)",
-    from: { transform: "translateY(100px)" },
-    config: animationConfig,
-  });
-
-  const leftAnimation = useSpring({
-    transform: "translateX(0px)",
-    from: { transform: "translateX(-100px)" },
+    transform: bottomInView ? "translateY(0px)" : "translateY(50px)",
+    from: { transform: "translateY(50px)" },
     config: animationConfig,
   });
 
   return (
     <div className="profile-info">
-      <animated.h2 className="mt-3" style={topAnimation}>
+      <animated.h2 className="mt-3" style={bottomAnimation}>
         {name}
       </animated.h2>
-      <animated.p className="mt-3 fw-semibold" style={rightAnimation}>
+      <animated.p className="mt-3 fw-semibold" style={bottomAnimation}>
         {role}
       </animated.p>
       <animated.p className="email-text" style={bottomAnimation}>
-        <a href={`mailto:${email}`} className="text-decoration-none text-black-50">{email}</a>
+        <a href={`mailto:${email}`} className="text-decoration-none text-black-50">
+          {email}
+        </a>
       </animated.p>
-      <animated.div className="extra-info" style={leftAnimation}>
-        {/* Add more content with left animation */}
+      <animated.div className="extra-info" style={bottomAnimation} ref={bottomRef}>
+        {/* Content that will be animated when it becomes visible */}
       </animated.div>
     </div>
   );
