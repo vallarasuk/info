da28744a585ba5a6608486f4775b48d91c718693
vallarasuk <vallarasuk143@gmail.com>
2023-09-26 02:31:33 +0530

    UI changed commit

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,11 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { useMediaQuery } from "react-responsive";
-import BaseNavbar from "./Components/Navbar/BaseNavbar";
+import Navbar from "./Components/Navbar/Navbar";
+// import Footer from "./Components/Footer/Footer";
 import "./Components/Styles/App.css";
 import "./Components/Styles/AppOne.css";
 import ProfileCard from "./Components/Lib/ProfileCard";
 import BottomNavbar from "./Components/Navbar/BottomNavbar";
+import NotFound from "./Components/Helper/NotFound";
+import ContentSection from "./Components/ContentSection";
+
+// Lazy load components
+const Home = lazy(() => import("./Components/Home"));
+const About = lazy(() => import("./Components/About"));
+const Projects = lazy(() => import("./Components/Projects"));
+const Contact = lazy(() => import("./Components/Contact"));
 
 const AppMain = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -17,51 +28,74 @@ const AppMain = () => {
     }, 2000); // Adjust the time as needed
   }, []);
 
-  const isSmallScreen = useMediaQuery({ maxWidth: 991 });
-  const isLargeScreen = useMediaQuery({ minWidth: 992 });
+  const isSmallScreen = useMediaQuery({ maxWidth: 991 }); // Adjust the max width for small screens
+  const isLargeScreen = useMediaQuery({ minWidth: 992 }); // Adjust the min width for large screens
 
   return (
     <HelmetProvider>
-      <div className="appOne-wrapper">
-        {isLargeScreen && <BaseNavbar />}
-        {isSmallScreen && <BottomNavbar />}
+      <Router>
+        <div className="appOne-wrapper">
+          {/* Navbar and BottomNavbar section */}
+          {/* {isLargeScreen && <Navbar />} */}
+          {/* {isSmallScreen && <BottomNavbar />} */}
 
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-lg-4">{isLargeScreen && <ProfileCard />}</div>
-            <div className="col-lg-8 px-lg-3 py-md-5">
-              <div className="split-screen-container pt-md-4">
-                <div className="loading-container">
-                  {isLoading ? (
-                    <div className="icon">
-                      <i className="fa fa-spinner fa-spin fa-3x text-black"></i>
-                    </div>
-                  ) : (
-                    <div>
-                      <div id="home">
-                        <h1>Home Section</h1>
-                        {/* Add content for the Home section */}
+          <div className="container-fluid">
+            <div className="row">
+              {/* Left side - Your profile info card */}
+              <div className="col-lg-4">{isLargeScreen && <ProfileCard />}</div>
+
+              {/* Right side - Other content */}
+              <div className="col-lg-8 px-lg-3 py-md-5">
+                <div className="split-screen-container pt-md-4">
+                  <Suspense
+                    fallback={
+                      <div className="loading-container">Loading...</div>
+                    }
+                  >
+                    {/* Add loading animation with icons */}
+                    {isLoading ? (
+                      <div className="loading-container">
+                        <div className="icon">
+                          {/* Use FontAwesome or any other icon library */}
+                          <i className="fa fa-spinner fa-spin fa-3x text-black"></i>
+                        </div>
                       </div>
-                      <div id="about">
-                        <h1>About Section</h1>
-                        {/* Add content for the About section */}
-                      </div>
-                      <div id="projects">
-                        <h1>Projects Section</h1>
-                        {/* Add content for the Projects section */}
-                      </div>
-                      <div id="contact">
-                        <h1>Contact Section</h1>
-                        {/* Add content for the Contact section */}
-                      </div>
-                    </div>
-                  )}
+                    ) : (
+                      // <Routes>
+                      //   <Route
+                      //     path="/"
+                      //     element={
+                      //       <TransitionGroup>
+                      //         <CSSTransition
+                      //           key="home"
+                      //           in={true}
+                      //           appear={true}
+                      //           timeout={500}
+                      //           classNames="fade"
+                      //         >
+                      //           <Home />
+                      //         </CSSTransition>
+                      //       </TransitionGroup>
+                      //     }
+                      //   />
+                      //   <Route path="/about" element={<About />} />
+                      //   <Route path="/projects" element={<Projects />} />
+                      //   <Route path="/contact" element={<Contact />} />
+                      //   {/* Add the 404 page */}
+                      //   <Route path="*" element={<NotFound />} />
+                      // </Routes>
+
+                      <ContentSection/>
+                    )}
+                  </Suspense>
                 </div>
+                {/* Footer section */}
+                {/* <Footer /> */}
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Router>
     </HelmetProvider>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import App from './App';
-import { library } from '@fortawesome/fontawesome-svg-core'; // Import Font Awesome library
-import { fab } from '@fortawesome/free-brands-svg-icons'; // Import Font Awesome brand icons
-import 'animate.css'; // Import Animate.css styles
-import './Components/Styles/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from "@fortawesome/fontawesome-svg-core"; // Import Font Awesome library
+import { fab } from "@fortawesome/free-brands-svg-icons"; // Import Font Awesome brand icons
+import "animate.css"; // Import Animate.css styles
+import "./Components/Styles/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-// import AppOne from './AppOne';
-import AppMain from './AppMain';
-
+import AppMain from "./AppMain";
+import AppOne from "./AppOne";
 
 // Add Font Awesome brand icons to the library
 library.add(fab);
@@ -18,7 +17,7 @@ ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <AppOne /> */}
-    <AppMain/>
+    <AppMain />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
