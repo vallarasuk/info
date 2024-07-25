036f766c60d1547eddbbd71a899dab9f7b591091
vallarasuk <vallarasu.kanthasamy@thidiff.com>
2024-07-25 20:53:31 +0530

    feature/styles minified version added

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production", // Ensure webpack runs in production mode
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+    }),
+  ],
+};
