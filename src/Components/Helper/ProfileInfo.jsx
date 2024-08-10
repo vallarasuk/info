1a669fae780bb5409ea9fb8d8dec8ff5adad12d1
vallarasuk <vallarasu.kanthasamy@thidiff.com>
2024-08-10 19:05:31 +0530

    role tag changed commit

--- src/Components/Helper/ProfileInfo.jsx
+++ src/Components/Helper/ProfileInfo.jsx
@@ -54,9 +54,9 @@ const ProfileInfo = ({ name, email }) => {
       <animated.h2 className="mt-3 my-name" style={bottomAnimation}>
         {name}
       </animated.h2>
-      <animated.h5 className="mt-4 fw-semibold my-role" style={roleAnimation}>
+      <animated.h6 className="mt-4 fw-semibold my-role h5" style={roleAnimation}>
         {currentRole}
-      </animated.h5>
+      </animated.h6>
       <animated.h5 className="email-text mt-4" style={bottomAnimation}>
         <a
           href={`mailto:${email}`}
